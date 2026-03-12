95149f5098baf31f5df69a213023472d9c3a9e08
ed0ard <2230272191@qq.com>
2026-03-12 13:49:32 +0000

    Removed admin permission requirement

--- CS2_ExecAfter/ExecAfterDelay.cs
+++ CS2_ExecAfter/ExecAfterDelay.cs
@@ -10,7 +10,6 @@ namespace CS2_ExecAfter
 	{
 
 		[ConsoleCommand("exec_after_delay", "Execute a command after a delay")]
-		[RequiresPermissions("@css/rcon")]
 		public void ConVarExecAfterDelay(CCSPlayerController? player, CommandInfo command)
 		{
 			if (command.ArgCount >= 3)
@@ -18,7 +17,6 @@ namespace CS2_ExecAfter
 				string delayArgs = command.ArgByIndex(1);
 				if (!float.TryParse(delayArgs, out float delayValue) || delayValue < 0)
 				{
-					ReplyToCommand($"Invalid value for exec_after_delay. Please specify a valid non-negative number. exec_after_delay <delay> \"<command>\"", player);
 					return;
 				}
 				string args = command.ArgString.Trim();
@@ -26,18 +24,15 @@ namespace CS2_ExecAfter
 				string commandToExecute = StripQuotes(args.Substring(trimChars, args.Length - trimChars));
 				if (string.IsNullOrWhiteSpace(commandToExecute))
 				{
-					ReplyToCommand($"Invalid value for exec_after_delay. Please specify a valid command to execute. exec_after_delay <delay> \"<command>\"", player);
 					return;
 				}
-				ReplyToCommand($"Executing \"{commandToExecute}\" in {delayValue} seconds.", player);
 				AddTimer(delayValue, () => {
-					ReplyToCommand($"Executing \"{commandToExecute}\"", player);
 					Server.ExecuteCommand(commandToExecute);
 				});
 			}
 			else
 			{
-				ReplyToCommand($"exec_after_delay <delay> \"<command>\"", player);
+				return;
 			}
 		}
 
