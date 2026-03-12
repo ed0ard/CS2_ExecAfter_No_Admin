4103ec8ff92ff2b5c861e198763e61bfed969f99
ed0ard <2230272191@qq.com>
2026-03-12 13:56:17 +0000

    Removed admin permission requirement and console log output

--- CS2_ExecAfter/ExecAfterRoundStart.cs
+++ CS2_ExecAfter/ExecAfterRoundStart.cs
@@ -18,12 +18,10 @@ namespace CS2_ExecAfter
 			{
 				if (!string.IsNullOrEmpty(exec_after_round_start))
 				{
-					ReplyToCommand($"Executing (round start): {exec_after_round_start}");
 					Server.ExecuteCommand(exec_after_round_start);
 				}
 				if (!string.IsNullOrEmpty(exec_after_round_start_once))
 				{
-					ReplyToCommand($"Executing once (round start): {exec_after_round_start_once}");
 					Server.ExecuteCommand(exec_after_round_start_once);
 					exec_after_round_start_once = null;
 				}
@@ -33,33 +31,27 @@ namespace CS2_ExecAfter
 
 
 		[ConsoleCommand("exec_after_round_start", "Executes a command after every round start")]
-		[RequiresPermissions("@css/rcon")]
 		public void ConVarExecAfterRoundStart(CCSPlayerController? player, CommandInfo command)
 		{
 			string args = command.ArgString.Trim();
 			if (string.IsNullOrEmpty(args))
 			{
-				ReplyToCommand($"exec_after_round_start = {exec_after_round_start}", player);
 				return;
 			}
 			args = StripQuotes(args);
 			exec_after_round_start = args;
-			ReplyToCommand($"exec_after_round_start = {exec_after_round_start}", player);
 		}
 
 		[ConsoleCommand("exec_after_round_start_once", "Executes a command after the next round start")]
-		[RequiresPermissions("@css/rcon")]
 		public void ConVarExecAfterRoundStartOnce(CCSPlayerController? player, CommandInfo command)
 		{
 			string args = command.ArgString.Trim();
 			if (string.IsNullOrEmpty(args))
 			{
-				ReplyToCommand($"exec_after_round_start_once = {exec_after_round_start_once}", player);
 				return;
 			}
 			args = StripQuotes(args);
 			exec_after_round_start_once = args;
-			ReplyToCommand($"exec_after_round_start_once = {exec_after_round_start_once}", player);
 		}
 
 	}
